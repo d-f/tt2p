f7fb824bf27dbdbce5734ae33a95a855079f010f
Tobias Markmann <tm@ayena.de>
2013-04-09 17:36:00 +0200

    Handle IDs in concrete cars.

--- src/main/java/roxelmaster2000/pojos/AbstractCar.java
+++ src/main/java/roxelmaster2000/pojos/AbstractCar.java
@@ -8,16 +8,5 @@ import com.gigaspaces.annotation.pojo.SpaceRouting;
 
 @SpaceClass
 abstract public class AbstractCar implements Serializable {
-	String id;
-	
-	@SpaceId(autoGenerate=true)
-    @SpaceRouting
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
 	public abstract boolean getEmpty();
 }
--- src/main/java/roxelmaster2000/pojos/Car.java
+++ src/main/java/roxelmaster2000/pojos/Car.java
@@ -2,6 +2,9 @@ package roxelmaster2000.pojos;
 
 import java.io.Serializable;
 
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceRouting;
+
 /**
  * Created with IntelliJ IDEA.
  * User: lucas
@@ -11,7 +14,18 @@ import java.io.Serializable;
  */
 
 public class Car extends AbstractCar {
+	String id;
+	
+	@SpaceId(autoGenerate=true)
+    @SpaceRouting
+	public String getId() {
+		return id;
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	@Override
 	public boolean getEmpty() {
 		return false;
